bfb6e05f4fe12beb909343686239a2515a6cbe37
Kingseungil <flykim511@naver.com>
2023-12-07 11:17:23 +0900

    gitbook test

---
+++ src/main/java/com/book/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.book.repository;
+
+import com.book.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+}
